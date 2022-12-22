9776b65a46a3526f5c5aa8a01936956f8c141472
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2022-12-23 00:02:08 +0530

    init commit

---
+++ client/src/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { configureStore } from "@reduxjs/toolkit";
+import App from "./App";
+import globalReducer from "state";
+import "./index.css";
+import { Provider } from "react-redux";
+import { api } from "state/api";
+import { setupListeners } from "@reduxjs/toolkit/dist/query";
+
+const store = configureStore({
+  reducer: { global: globalReducer, [api.reducerPath]: api.reducer },
+  middleware: (getDefault) => getDefault().concat(api.middleware),
+});
+setupListeners(store.dispatch);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
---
+++ server/index.js
@@ -0,0 +1,64 @@
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+import cors from "cors";
+import clientRoutes from "./routes/client.js";
+import generalRoutes from "./routes/general.js";
+import managementRoutes from "./routes/management.js";
+import salesRoutes from "./routes/sales.js";
+import mongoose from "mongoose";
+
+// DATA IMPORTS
+import User from "./models/User.js";
+import Product from "./models/Product.js";
+import ProductStat from "./models/ProductStat.js";
+import Transaction from "./models/Transaction.js";
+import OverallStat from "./models/OverallStat.js";
+import AffiliateStat from "./models/AffiliateStat.js";
+import {
+  dataOverallStat,
+  dataProduct,
+  dataProductStat,
+  dataTransaction,
+  dataUser,
+  dataAffiliateStat,
+} from "./data/index.js";
+
+// CONFIGURATION
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+// ROUTES
+app.use("/client", clientRoutes);
+app.use("/general", generalRoutes);
+app.use("/management", managementRoutes);
+app.use("/sales", salesRoutes);
+
+// MONGOOSE SETUP
+const PORT = process.env.PORT || 9000;
+mongoose
+  .set("strictQuery", true)
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+    // ONLY ADD DATA ONE TIME
+    // AffiliateStat.insertMany(dataAffiliateStat);
+    // OverallStat.insertMany(dataOverallStat);
+    // Product.insertMany(dataProduct);
+    // ProductStat.insertMany(dataProductStat);
+    // Transaction.insertMany(dataTransaction);
+  })
+  .catch((error) => console.log(`${error} did not connect`));
